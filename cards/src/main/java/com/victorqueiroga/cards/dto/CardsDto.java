45844da8ab008a08459e6f7ea6326ecd5ae9ccf0
Victor Queiroga <victorqueiroga.jp@gmail.com>
2025-07-13 14:25:43 -0300

    Including Cards and Loans microservices

---
+++ cards/src/main/java/com/victorqueiroga/cards/dto/CardsDto.java
@@ -0,0 +1,56 @@
+package com.victorqueiroga.cards.dto;
+
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.*;
+
+@Schema(name = "Cards",
+        description = "Schema to hold Card information"
+)
+@Data
+public class CardsDto {
+
+    @NotEmpty(message = "{cards.mobileNumber.empty}")
+    @Pattern(regexp="(^$|[0-9]{10})",message = "{cards.mobileNumber.pattern}")
+    @Schema(
+            description = "Mobile Number of Customer", example = "4354437687"
+    )
+    private String mobileNumber;
+
+    @NotEmpty(message = "{cards.cardNumber.empty}")
+    @Pattern(regexp="(^$|[0-9]{12})",message = "{cards.cardNumber.pattern}")
+    @Schema(
+            description = "Card Number of the customer", example = "100646930341"
+    )
+    private String cardNumber;
+
+    @NotEmpty(message = "{cards.cardType.empty}")
+    @Schema(
+            description = "Type of the card", example = "Credit Card"
+    )
+    private String cardType;
+
+    @Positive(message = "{cards.totalLimit.positive}")
+    @Schema(
+            description = "Total amount limit available against a card", example = "100000"
+    )
+    private int totalLimit;
+
+    @PositiveOrZero(message = "{cards.amountUsed.positive}")
+    @Schema(
+            description = "Total amount used by a Customer", example = "1000"
+    )
+    private int amountUsed;
+
+    @PositiveOrZero(message = "{cards.availableAmount.positive}")
+    @Schema(
+            description = "Total available amount against a card", example = "90000"
+    )
+    private int availableAmount;
+
+}
---
+++ loans/src/main/java/com/victorqueiroga/loans/dto/LoansDto.java
@@ -0,0 +1,54 @@
+package com.victorqueiroga.loans.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.*;
+
+@Schema(name = "Loans",
+        description = "Schema to hold Loan information"
+)
+@Data
+public class LoansDto {
+
+    @NotEmpty(message = "{loans.mobileNumber.empty}")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "{loans.mobileNumber.pattern}")
+    @Schema(
+            description = "Mobile Number of Customer", example = "4365327698"
+    )
+    private String mobileNumber;
+
+    @NotEmpty(message = "{loans.loanNumber.empty}")
+    @Pattern(regexp = "(^$|[0-9]{12})", message = "{loans.loanNumber.pattern}")
+    @Schema(
+            description = "Loan Number of the customer", example = "548732457654"
+    )
+    private String loanNumber;
+
+    @NotEmpty(message = "{loans.loanType.empty}")
+    @Schema(
+            description = "Type of the loan", example = "Home Loan"
+    )
+    private String loanType;
+
+    @Positive(message = "{loans.totalLoan.positive}")
+    @Schema(
+            description = "Total loan amount", example = "100000"
+    )
+    private int totalLoan;
+
+    @PositiveOrZero(message = "{loans.amountPaid.positive_or_zero}")
+    @Schema(
+            description = "Total loan amount paid", example = "1000"
+    )
+    private int amountPaid;
+
+    @PositiveOrZero(message = "{loans.outstandingAmount.positive_or_zero}")
+    @Schema(
+            description = "Total outstanding amount against a loan", example = "99000"
+    )
+    private int outstandingAmount;
+
+}
