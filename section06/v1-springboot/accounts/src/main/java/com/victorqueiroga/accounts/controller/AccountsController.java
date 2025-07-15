258e7a4e9589bef26bf40a2adba689a8f0bbe84c
queirogavictor-at-892414404840 <victorqueiroga.jp@gmail.com>
2025-07-15 15:56:19 -0300

    section 06 - cap. 76

--- section06/v1-springboot/accounts/src/main/java/com/victorqueiroga/accounts/controller/AccountsController.java
+++ section06/v1-springboot/accounts/src/main/java/com/victorqueiroga/accounts/controller/AccountsController.java
@@ -1,6 +1,7 @@
 package com.victorqueiroga.accounts.controller;
 
 import com.victorqueiroga.accounts.constants.AccountsConstants;
+import com.victorqueiroga.accounts.dto.AccountsContactInfoDto;
 import com.victorqueiroga.accounts.dto.CustomerDto;
 import com.victorqueiroga.accounts.dto.ErrorResponseDto;
 import com.victorqueiroga.accounts.dto.ResponseDto;
@@ -14,6 +15,11 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -23,11 +29,23 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "Crud Rest APIs for Accounts in EasyBank", description = "CRUD Rest APIs for Accounts in EasyBank to create, fetch, update and delete account details")
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
-@AllArgsConstructor
 @Validated
 public class AccountsController {
 
     private final IAccountsService accountsService;
+
+    @Value("${build.version}")
+    private String buildVersion;
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private AccountsContactInfoDto accountsContactInfoDto;
+
+    public AccountsController(IAccountsService accountsService) {
+        this.accountsService = accountsService;
+    }
 
     @Operation(
             summary = "Create new account",
@@ -166,5 +184,106 @@ public class AccountsController {
             return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(AccountsConstants.STATUS_200, "Success"));
         }
         return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_UPDATE));
+    }
+
+    @Operation(
+            summary = "Get build version",
+            description = "Get build version in EasyBank"
+    )
+    @ApiResponses(
+            {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Success",
+                            content = {
+                                    @Content(
+                                            schema = @Schema(
+                                                    implementation = String.class,
+                                                    example = "1.0.0"
+
+                                            )
+                                    )
+                            }
+
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal Server Error",
+                            content = {
+                                    @Content(
+                                            schema = @Schema(
+                                                    implementation = ErrorResponseDto.class
+                                            )
+                                    )
+                            }
+                    )
+            }
+    )
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion() {
+        String javaVersion = environment.getProperty("JAVA_HOME");
+        return ResponseEntity.status(HttpStatus.OK).body(javaVersion); } // <1>
+
+    @Operation(
+            summary = "Get build version",
+            description = "Get build version in EasyBank"
+    )
+    @ApiResponses(
+            {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Success",
+                            content = {
+                                    @Content(
+                                            schema = @Schema(
+                                                    implementation = String.class,
+                                                    example = "1.0.0"
+
+                                            )
+                                    )
+                            }
+
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal Server Error",
+                            content = {
+                                    @Content(
+                                            schema = @Schema(
+                                                    implementation = ErrorResponseDto.class
+                                            )
+                                    )
+                            }
+                    )
+            }
+    )
+    @GetMapping("/build-info")
+    public ResponseEntity<String> getBuildVersion() {
+              return ResponseEntity.status(HttpStatus.OK).body(buildVersion); // <2>
+    }
+
+    @Operation(
+            summary = "Get contact info",
+            description = "Get contact info that can be reached out in case of any issues"
+    )
+    @ApiResponses(
+            {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Success",
+                            content = {
+                                    @Content(
+                                            schema = @Schema(
+                                                    implementation = AccountsContactInfoDto.class
+
+                                            )
+                                    )
+                            }
+                    )
+            }
+    )
+    @GetMapping("/contact-info")
+    public ResponseEntity<AccountsContactInfoDto> getAccountsContactInfo() {
+        return ResponseEntity.status(HttpStatus.OK).body(accountsContactInfoDto);
     }
 }
